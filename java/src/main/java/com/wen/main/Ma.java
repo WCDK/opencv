a721ce734ced11e32565582a22b37095a15d623b
wcdk <wenchuanmail@vip.qq.com>
2022-06-09 17:11:18 +0800

    单行字符顺序调整

--- java/src/main/java/com/wen/main/Ma.java
+++ java/src/main/java/com/wen/main/Ma.java
@@ -13,11 +13,12 @@ public class Ma {
 
 
     public static void main(String[] paramArrayOfString) throws UnsupportedEncodingException {
+
         String base = System.getProperty("user.dir");
         String path = base+"\\file\\3.jpg";
-        System.out.println(path);
         String knpath=base+"\\KN";
         Ma m = new Ma();
+        m.test();
         String distinguish = m.distinguish(path,knpath);
         System.out.println(distinguish);
     }
